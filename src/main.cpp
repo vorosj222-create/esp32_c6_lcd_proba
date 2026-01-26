dc78ac9ef148b4b4517d5e24ac4dd9d3c8720145
vorosj222-create <vorosj222@gmail.com>
2026-01-26 04:27:50 +0100

    cleaning code

--- src/main.cpp
+++ src/main.cpp
@@ -3,95 +3,74 @@
 #include <SPI.h>
 #include <U8g2_for_Adafruit_GFX.h>
 
-// GPIO kiosztás az IDF kódod alapján
+// GPIO pinout
 #define TFT_MOSI 6
 #define TFT_SCLK 7
 #define TFT_CS 14
 #define TFT_DC 15
 #define TFT_RST 21
-#define TFT_BL 22 // Háttérvilágítás
+#define TFT_BL 22 // background illumination
 
-// Kijelző paraméterei az IDF kódod alapján
+// Display
 #define LCD_W 320
 #define LCD_H 172
 #define OFFSET_X 0
 #define OFFSET_Y 34
 
-// Kijelző objektum létrehozása
+// Display object
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
 
-// U8g2 adapter példány
+// U8g2 adapter
 U8G2_FOR_ADAFRUIT_GFX u8g2;
 
 void drawTestScreen()
 {
-  // Piros sáv (felül)
-  tft.fillRect(0, 0, 320, 22, ST77XX_BLUE);
+   // Kék sáv (középen)
+  tft.fillRect(0, 0, 320, 172, ST77XX_BLUE);
 
-  // Kék sáv (középen)
-  tft.fillRect(0, 22, 320, 64, ST77XX_BLUE);
-
-  // Zöld sáv (alul)
-  tft.fillRect(0, 86, 320, 86, ST77XX_BLUE);
-
-  u8g2.setForegroundColor(ST77XX_WHITE); // wrapper saját színbeállítás
-  u8g2.setBackgroundColor(ST77XX_BLUE);  // wrapper saját színbeállítás
+  u8g2.setForegroundColor(ST77XX_WHITE); // wrapper color
+  u8g2.setBackgroundColor(ST77XX_BLUE);  
 
   // Példa font beállítása https://github.com/olikraus/u8g2/wiki/fntlist99
-  u8g2.setFont(u8g2_font_inb46_mf); // U8g2 font
-
+  //u8g2.setFont(u8g2_font_inb46_mf); // U8g2 font
+  u8g2.setFont(u8g2_font_fub35_tf);
   // Kiírás U8g2 fonttal
-  u8g2.setCursor(10, 80);
-  u8g2.print("7011.456");
+  u8g2.setCursor(14, 80);
+  u8g2.print("Hello world!");
 
-  // Szöveg a kék sávba
-  tft.setCursor(15, 45);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(6);
-  // tft.print("7025.521");
-
-  tft.setCursor(25, 110);
+  tft.setCursor(16, 110);
   tft.setTextColor(ST77XX_WHITE);
   tft.setTextSize(4);
-  tft.print("40m");
-
-  tft.setCursor(240, 110);
-  tft.setTextColor(ST77XX_WHITE);
-  tft.setTextSize(4);
-  tft.print("CW");
+  tft.print("Hello world!");
 }
 
 void setup()
 {
   Serial.begin(115200);
 
-  // Háttérvilágítás bekapcsolása
+  // Background illumination ON
   pinMode(TFT_BL, OUTPUT);
   digitalWrite(TFT_BL, HIGH);
 
-  // SPI inicializálása az egyedi lábakon (SCLK, MISO (-1), MOSI, CS)
+  // SPI init (SCLK, MISO (-1), MOSI, CS)
   SPI.begin(TFT_SCLK, -1, TFT_MOSI, TFT_CS);
 
-  // ST7789 inicializálása
-  // Az init(szélesség, magasság) után manuálisan állítjuk be az elforgatást és az offsetet
-  tft.init(LCD_H, LCD_W); // Felcseréljük, mert az IDF-ben swap_xy = true volt
+  // ST7789 init
+  // Init(width, height) then turning, offset
+  tft.init(LCD_H, LCD_W); // replace, because swap_xy = true volt
 
-  // Színek invertálása és elforgatás (az IDF esp_lcd_panel_invert_color alapján)
+  // Color invert and rotation
   tft.invertDisplay(true);
-  tft.setRotation(1); // Fekvő mód
+  tft.setRotation(1); // landscape
 
   u8g2.begin(tft);
 
-  // A GAP (offset) kezelése: az Adafruit GFX-ben ezt néha manuálisan kell korrigálni,
-  // ha a kijelző nem a 0,0 sarokból indul.
-  // Az ST7789 könyvtárban ez az eltolás automatikusan alkalmazva lesz, ha jó típust választunk:
   tft.fillScreen(ST77XX_BLACK);
 
-  // Teszt rajzolás
+  // Test
   drawTestScreen();
 }
 
 void loop()
 {
-  // Itt tarthatod a frissítéseket
 }
